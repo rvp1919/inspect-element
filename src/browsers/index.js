9909d8b68a9da7213be1ea0cd7abb6871009ab69
Ernest <iendeavor.github.io@gmail.com>
2020-11-09 21:58:59 +0800

    refactor: remove redundant files

--- src/browsers/index.js
+++
@@ -1,9 +0,0 @@
-import { get as chromeGet, set as chromeSet } from './chrome'
-
-export function get(key) {
-  return chromeGet(key)
-}
-
-export function set(key, value) {
-  return chromeSet(key, value)
-}
--- src/contentScript/style.js
+++
@@ -1,6 +0,0 @@
-export const className = 'inspect-element'
-
-// Create and append style node for the hover element.
-export function appendStyleNode() {
-  throw Error('Not yet implemented.')
-}
