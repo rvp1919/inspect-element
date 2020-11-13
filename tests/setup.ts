dd5f79412c2848bb57010fcb32112eccafb6ae9a
Ernest <iendeavor.github.io@gmail.com>
2020-11-13 16:36:30 +0800

    refactor: remove redundant code

--- tests/setup.ts
+++ tests/setup.ts
@@ -5,22 +5,15 @@ import type { Browser } from 'puppeteer'
 
 const pathToExtension = path.join(__dirname, '../dist')
 
-export function launch(times = 0): Browser {
-  return (() => {
-    try {
-      return puppeteerLaunch({
-        executablePath: process.env.PUPPETEER_EXEC_PATH,
-        headless: false,
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          `--disable-extensions-except=${pathToExtension}`,
-          `--load-extension=${pathToExtension}`,
-        ],
-      })
-    } catch (error) {
-      if (times === 5) throw Error(error)
-      else return launch(times + 1)
-    }
-  })() as Browser
+export function launch(): Promise<Browser> {
+  return puppeteerLaunch({
+    executablePath: process.env.PUPPETEER_EXEC_PATH,
+    headless: false,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      `--disable-extensions-except=${pathToExtension}`,
+      `--load-extension=${pathToExtension}`,
+    ],
+  })
 }
